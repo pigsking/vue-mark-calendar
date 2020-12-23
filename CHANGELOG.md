29ebee2befe074329f45956a29cb2165255f272b
allenou <oyqxiao@sina.com>
2020-12-23 22:31:32 +0800

    release 1.0.0-alpha.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,18 @@
 # 更新日志
 
+## 1.0.0-alpha.0
+
+### Features
+- 移动端支持滑动切换年月
+- 一次固定显示 42 天
+- 支持年月选择
+
+### Code Refactoring
+- 使用 Vite 替换 Vue CLI
+
+### BREAKING CHANGES
+- 将 Vue2.x 更新至 Vux3.x
+
 ## v0.7.0
 
 ### Bug Fixes
--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 // import Calendar from "../packages";
-import Calendar from '../dist/vue-mark-calendar.es.js'
-// import Calendar from '@pigsking/vue-mark-calendar'
+// import Calendar from '../dist/vue-mark-calendar.es.js'
+import Calendar from '@pigsking/vue-mark-calendar'
 
 const app = createApp(App)
 app.use(Calendar)
